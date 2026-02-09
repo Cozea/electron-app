874599105331b84fbebf77ef8a86da0f8f3e09dc
Kelyan Othniel Edou Engone <kelyan@crossand.ai>
2026-02-09 16:16:13 +0800

    Adjust conversation button and list

--- src/components/ai-elements/conversation.tsx
+++ src/components/ai-elements/conversation.tsx
@@ -85,7 +85,7 @@ export const ConversationScrollButton = ({
   return (
     <Button
       className={cn(
-        "absolute bottom-4 left-[50%] translate-x-[-50%] rounded-full dark:bg-background dark:hover:bg-muted",
+        "absolute bottom-4 left-[50%] translate-x-[-50%] rounded-full border border-border/80 dark:bg-background dark:hover:bg-muted",
         className,
       )}
       onClick={handleScrollToBottom}
